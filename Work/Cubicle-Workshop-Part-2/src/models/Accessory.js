890e3204d21ebcf4dfe232964091e07a50df33ed
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-02-13 14:36:07 +0200

    Create Accessory.js

---
+++ Work/Cubicle-Workshop-Part-2/src/models/Accessory.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const accessorySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate:{
+            validator: function (value) {
+                return value.startsWith('http://') || value.startsWith('https://');
+            },
+            message: `Url invalid!`
+        }
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 100
+    }
+});
+
+const Accessory = model('Accessory', accessorySchema);
+
+module.exports = Accessory;
