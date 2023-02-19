abf7b4d2771891e88d46c4aa777886009c6ed8f5
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-02-19 12:05:59 +0200

    Create authManager.js

---
+++ Exam/02. Book Talk/manager/authManager.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+const jwt = require('../lib/jsonwebtoken');
+const SECRET = require('../config');
+
+exports.usernameChecker = (username) => User.findOne( {username} );
+
+exports.emailChecker = (email) => User.findOne({ email });
+
+exports.register = async (email, username, password) => {
+    
+    try {
+        const user = await User.create({ email, username, password });
+
+        const hash = await bcrypt.hash(password, 10);
+        password = hash;
+    
+        await user.updateOne( { password: hash });
+    
+        return user;
+    } catch(err) {
+        throw err;
+    }
+    
+};
+
+exports.login = async (email, password) => {
+
+    if (email === '' || password === '') {
+        throw 'All field\'s are required!';
+    }
+
+    const validEmail = await this.emailChecker(email).lean();
+
+    if (!validEmail) {
+        throw 'Invalid email or password!';
+    }
+
+    const validPassword = await bcrypt.compare(password, validEmail.password);;
+
+    if (!validPassword) {
+        throw 'Invalid email or password!';
+    }
+
+    const payload = { username: validEmail.username, id: validEmail._id.toString(), email: validEmail.email };
+    const token = jwt.sign(payload, SECRET, { expiresIn: '1d' });
+
+    return token;
+}
