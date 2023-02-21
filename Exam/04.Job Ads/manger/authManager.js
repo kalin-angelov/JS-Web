b17338560bc5c0d79e9b167b72f88ff4502560a9
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-02-21 20:00:33 +0200

    Create authManager.js

---
+++ Exam/04.Job Ads/manger/authManager.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+const jwt = require('../lib/jsonwebtoken');
+const SECRET = require('../config');
+
+exports.emailChecker = (email) => User.findOne({ email });
+
+exports.register = async (email, password, skill) => {
+    
+    try {
+        const user = await User.create({ email, password, skill });
+
+        const hash = await bcrypt.hash(password, 10);
+        password = hash;
+    
+        await user.updateOne( { password: hash });
+    
+        return user;
+    } catch(err) {
+        throw err;
+    }
+    
+};
+
+exports.login = async (email, password) => {
+
+    if (email === '' || password === '') {
+        throw 'All field\'s are required!';
+    }
+
+    const validEmail = await this.emailChecker(email).lean();
+
+    if (!validEmail) {
+        throw 'Invalid email or password!';
+    }
+
+    const validPassword = await bcrypt.compare(password, validEmail.password);
+
+    if (!validPassword) {
+        throw 'Invalid email or password!';
+    }
+
+    const payload = { email: validEmail.email, id: validEmail._id.toString() };
+    const token = jwt.sign(payload, SECRET, { expiresIn: '1d' });
+
+    return token;
+};
