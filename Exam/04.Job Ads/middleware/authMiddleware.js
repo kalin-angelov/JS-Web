aca0f588bedb24ea3c75f35dfb7aaa9d3c995c1b
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-02-21 20:01:23 +0200

    Create authMiddleware.js

---
+++ Exam/04.Job Ads/middleware/authMiddleware.js
@@ -0,0 +1,45 @@
+const jwt = require('../lib/jsonwebtoken');
+const SECRET = require('../config');
+
+const Ads = require('../models/Ads');
+
+exports.authentication = async (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if(token) {
+        try {
+            const decodeToken = await jwt.verify(token, SECRET);
+
+            req.user = decodeToken;
+            req.isAuthenticated = true;
+
+            res.locals.username = decodeToken.username;
+            res.locals.isAuthenticated = true;
+        } catch(err) {
+            res.clearCookie('auth');
+            return  res.redirect('/404');
+        }
+    }
+
+    next();
+};
+
+exports.isAuthenticated = (req, res, next) => {
+    if (!req.isAuthenticated) {
+        return res.redirect('/404');
+    }
+
+    next();
+};
+
+exports.isAuthorized = async (req, res, next) => {
+    const userId = req.user.id;
+    const productId = req.params.id;
+
+    const product = await Ads.findById(productId).populate('author').lean();
+    if(!product.author.some(product => product._id.toString() === userId)) {
+        return res.redirect('/404');
+    }
+
+    next();
+};
