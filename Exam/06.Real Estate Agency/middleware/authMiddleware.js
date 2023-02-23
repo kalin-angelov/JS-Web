0211962ac0db4f178b24658efb7c5c4f705686de
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-02-23 18:06:29 +0200

    Create authMiddleware.js

---
+++ Exam/06.Real Estate Agency/middleware/authMiddleware.js
@@ -0,0 +1,45 @@
+const jwt = require('../lib/jsonwebtoken');
+const SECRET = require('../config');
+
+const Housing = require('../models/Housing');
+
+exports.authentication = async (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if(token) {
+        try {
+            const decodeToken = await jwt.verify(token, SECRET);
+
+            req.user = decodeToken;
+            req.isAuthenticated = true;
+
+            res.locals.username = decodeToken.username;
+            res.locals.isAuthenticated = true;
+        } catch(err) {
+            res.clearCookie('auth');
+            return  res.redirect('/404');
+        }
+    }
+
+    next();
+};
+
+exports.isAuthenticated = (req, res, next) => {
+    if (!req.isAuthenticated) {
+        return res.redirect('/404');
+    }
+
+    next();
+};
+
+exports.isAuthorized = async (req, res, next) => {
+    const userId = req.user.id;
+    const productId = req.params.id;
+
+    const product = await Housing.findById(productId).populate('owner').lean();
+    if(!product.owner.some(product => product._id.toString() === userId)) {
+        return res.redirect('/404');
+    }
+
+    next();
+};
